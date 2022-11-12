415b7ca2d7dcbba22ef929b968b1a0edf0065402
khine-phyu <110364741+khine-phyu@users.noreply.github.com>
2022-11-12 17:59:57 +0630

    create profile, register component

--- shopping_cart/src/App.vue
+++ shopping_cart/src/App.vue
@@ -3,11 +3,12 @@
     <v-main>
       <!-- nav -->
       <v-app-bar color="deep-purple accent-4" dark>
-        <v-btn class="text-h5" text href="/">Home</v-btn>
+        <v-btn class="text-h5" text @click="gotoScreen('/')">Home</v-btn>
         <v-spacer></v-spacer>
-        <v-btn text href="/register">Register</v-btn>
-        <v-btn text href="/profile">Profile</v-btn>
-        <v-btn text href="/about">About</v-btn>
+        <v-btn v-show="!isRegister" text @click="gotoScreen('/register')">Register</v-btn>
+        <v-btn v-show="isRegister" text @click="gotoScreen('/profile')">Profile</v-btn>
+        <v-btn text @click="gotoScreen('/about')">About</v-btn>
+        <v-btn v-show="isRegister" text @click="onClickLogout">Logout</v-btn>
       </v-app-bar>
       <!-- nav -->
       <v-container>
@@ -23,9 +24,40 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
-  })
-}
+    isRegister: false,
+  }),
+
+  created() {
+    this.isRegister = this.$store.state.isRegister;
+    this.$store.watch(
+      () => {
+        return this.$store.state.isRegister;
+      },
+      (newVal, oldVal) => {
+        this.isRegister = newVal;
+      },
+      {
+        deep: true,
+      }
+    );
+  },
+
+  methods: {
+    onClickLogout() {
+      this.$store.commit('setIsRegister', false);
+      this.$store.commit('setUserInfo', {});
+
+      //Go to home
+      if(this.$router.currentRoute.path != "/") {
+        this.gotoScreen("/");
+      }
+    },
+
+    gotoScreen(screenPath) {
+      this.$router.push({path: screenPath});
+    }
+  }
+};
 </script>
 
 <style>
--- shopping_cart/src/views/Home.vue
+++ shopping_cart/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Welcome to Home</h1>
+        <h1>Welcome to Home {{ userInfo.name }}</h1>
     </div>
 </template>
 
@@ -8,5 +8,27 @@
 export default {
     name: "home",
     components: {},
+
+    data() {
+        return {
+            userInfo: {},
+        };
+    },
+
+    created() {
+        // console.log(this.$router.currentRoute.path);
+        this.userInfo = this.$store.state.userInfo;
+        this.$store.watch(
+            () => {
+                return this.$store.state.userInfo;
+            },
+            (newVal, oldVal) => {
+                this.userInfo = newVal;
+            },
+            {
+                deep: true,
+            }
+        );
+    },
 }
 </script>
