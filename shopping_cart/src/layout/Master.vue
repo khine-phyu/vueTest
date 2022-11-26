b8366671ccc81a7898a6de246a0665e6b572b00a
khine-phyu <110364741+khine-phyu@users.noreply.github.com>
2022-11-26 17:35:45 +0630

    new component and nav

---
+++ shopping_cart/src/layout/Master.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+        <!-- Navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            <a href="#">
+                                <i class="fas fa-cart-arrow-down"></i>
+                                <small class="badge badge-danger" style="font-size: 8px">9</small>
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </nav>
+
+        <!-- Content  -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card p-3 m-3">
+                        <div class="card-header">Product Name</div>
+                        <div class="card-body">
+                            <img src="https://www.alibaba.com/product-detail/Best-Selling-Women-s-Swim-Suits_10000007506225.html?spm=a2700.8293689.youMayAlsoLike.1.441867af2sXaL7" alt="">
+                        </div>
+                        <div class="card-footer">
+                            <span>Price: <small>$100</small></span>
+                            <a href="" class="btn btn-sm btn-dark float-right">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 m-3">
+                        <div class="card-header">Product Name</div>
+                        <div class="card-body">
+                            <img src="https://www.alibaba.com/product-detail/Best-Selling-Women-s-Swim-Suits_10000007506225.html?spm=a2700.8293689.youMayAlsoLike.1.441867af2sXaL7" alt="">
+                        </div>
+                        <div class="card-footer">
+                            <span>Price: <small>$100</small></span>
+                            <a href="" class="btn btn-sm btn-dark float-right">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 m-3">
+                        <div class="card-header">Product Name</div>
+                        <div class="card-body">
+                            <img src="https://www.alibaba.com/product-detail/Best-Selling-Women-s-Swim-Suits_10000007506225.html?spm=a2700.8293689.youMayAlsoLike.1.441867af2sXaL7" alt="">
+                        </div>
+                        <div class="card-footer">
+                            <span>Price: <small>$100</small></span>
+                            <a href="" class="btn btn-sm btn-dark float-right">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 m-3">
+                        <div class="card-header">Product Name</div>
+                        <div class="card-body">
+                            <img src="https://www.alibaba.com/product-detail/Best-Selling-Women-s-Swim-Suits_10000007506225.html?spm=a2700.8293689.youMayAlsoLike.1.441867af2sXaL7" alt="">
+                        </div>
+                        <div class="card-footer">
+                            <span>Price: <small>$100</small></span>
+                            <a href="" class="btn btn-sm btn-dark float-right">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card p-3 m-3">
+                        <div class="card-header">Product Name</div>
+                        <div class="card-body">
+                            <img src="https://www.alibaba.com/product-detail/Best-Selling-Women-s-Swim-Suits_10000007506225.html?spm=a2700.8293689.youMayAlsoLike.1.441867af2sXaL7" alt="">
+                        </div>
+                        <div class="card-footer">
+                            <span>Price: <small>$100</small></span>
+                            <a href="" class="btn btn-sm btn-dark float-right">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Content  -->
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'master'
+}
+</script>
+
+<style>
+    .card-body img {
+        width: 250px;
+        height: 250px;
+    }
+    .float-right {
+        float: right;
+    }
+</style>
