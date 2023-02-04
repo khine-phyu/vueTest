e88b7831d12090750ead0e9ce234177dcdf5a290
khine-phyu <110364741+khine-phyu@users.noreply.github.com>
2023-02-04 17:46:52 +0630

    css folder

---
+++ vue0402/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Heading></Heading>
+    <Footing></Footing>
+  </div>
+</template>
+
+<script>
+import Heading from "./components/Heading.vue";
+import Footing from "./components/Footing.vue";
+
+export default {
+  name: "App",
+  components: {
+    Heading,
+    Footing,
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/heading.css";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ vue0402/src/components/Footing.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Footing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footing",
+};
+</script>
+
+<style>
+</style>
