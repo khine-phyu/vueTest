d26d50b312d481fc7ce46b884888feaab90ff31a
khine-phyu <110364741+khine-phyu@users.noreply.github.com>
2023-02-07 17:46:32 +0630

    slider

--- vue0402/src/components/top.vue
+++ vue0402/src/components/top.vue
@@ -1,22 +1,50 @@
 <template>
-  <div>
+  <div class="top-page">
     <!-- <Heading></Heading> -->
-    <div class="mv">
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
-        consequuntur doloribus et autem officiis reiciendis mollitia, quam nihil
-        aliquid molestiae similique possimus expedita amet soluta totam facilis
-        numquam debitis natus.
-      </p>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3819.9513308449214!2d96.1521369143454!3d16.77909682448044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c1ec821e07a833%3A0xdde741e3cd511209!2sJunction%20City!5e0!3m2!1sen!2smm!4v1675674513423!5m2!1sen!2smm"
-        width="600"
-        height="450"
-        style="border: 0"
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
+    <div class="inn">
+      <div class="mv">
+        <!-- <div id="app"> -->
+        <div class="buttons">
+          <button @click="removeSlide" :disabled="slides <= 2">
+            Remove Slide
+          </button>
+          <button @click="addSlide" :disabled="slides >= 10">Add Slide</button>
+        </div>
+        <div class="slides">
+          <transition-group
+            name="slide"
+            mode="out-in"
+            enter-class="slide-in"
+            leave-class="slide-out"
+            enter-active-class="animated slide-in-active"
+            leave-active-class="animated slide-out-active"
+          >
+            <div v-for="index in slides" v-if="index == active" :key="index">
+              Slide {{ index }}
+            </div>
+          </transition-group>
+        </div>
+        <span class="prev" @click="move(-1)">
+          <i class="fa fa-chevron-left" aria-hidden="true"></i>
+        </span>
+        <span class="next" @click="move(1)">
+          <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </span>
+        <ul class="dots">
+          <li
+            v-for="(dot, index) in slides"
+            :class="{ active: ++index === active }"
+            @click="jump(index)"
+          ></li>
+        </ul>
+        <!-- </div> -->
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
+          consequuntur doloribus et autem officiis reiciendis mollitia, quam
+          nihil aliquid molestiae similique possimus expedita amet soluta totam
+          facilis numquam debitis natus.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -28,5 +56,27 @@ export default {
   //   components: {
   //     Heading,
   //   },
+  data: {
+    slides: 5,
+    active: 1,
+  },
+  methods: {
+    move(amount) {
+      let newActive;
+      const newIndex = this.active + amount;
+      if (newIndex > this.slides) newActive = 1;
+      if (newIndex === 0) newActive = this.slides;
+      this.active = newActive || newIndex;
+    },
+    jump(index) {
+      this.active = index;
+    },
+    addSlide() {
+      this.slides = this.slides + 1;
+    },
+    removeSlide() {
+      this.slides = this.slides - 1;
+    },
+  },
 };
 </script>
